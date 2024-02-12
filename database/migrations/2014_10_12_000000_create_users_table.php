0688934cdc76afa69cb18ba3b18ee7593472477f
MaliqourHuncho254 <maliqourhuncho@gmail.com>
2024-02-12 21:31:27 +0300

    Actually this is the start of my blogproject of which am startng today as we are going valentine season

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            //This is what we have been able to change
+            $table->string('name');
             $table->rememberToken();
             $table->timestamps();
         });
